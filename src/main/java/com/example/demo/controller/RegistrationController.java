970a009570e8cc56039ca2231645d40dfcfd9709
el1191 <el1191@woyd.ru>
2020-05-08 00:50:10 +0300

    RegistrationController: Prettify

--- src/main/java/com/example/demo/controller/RegistrationController.java
+++ src/main/java/com/example/demo/controller/RegistrationController.java
@@ -21,25 +21,21 @@ public class RegistrationController {
     @Autowired
     private UserRepository userRepository;
 
-    public RegistrationController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "successful operation",
             content = @Content(schema = @Schema(implementation = User.class)))
     })
     @PostMapping("/registration")
-    public Boolean create(@RequestBody Map<String, String> body) throws NoSuchAlgorithmException {
+    public Boolean create(@RequestBody Map<String, String> body) {
         String username = body.get("username");
         if (userRepository.existsByUsername(username)){
             throw new ValidationException("Username already existed");
         }
-        String full_name = body.get("full_name");
+        String fullName = body.get("full_name");
         String email = body.get("email");
         String password = body.get("password");
         String encodedPassword = new BCryptPasswordEncoder().encode(password);
-        userRepository.save(new User(username, encodedPassword, full_name, email));
+        userRepository.save(new User(username, encodedPassword, fullName, email));
         return true;
     }
 }
