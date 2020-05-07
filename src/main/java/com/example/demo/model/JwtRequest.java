d22cee7128734fe1b4db32925c85066561d0c19b
el1191 <el1191@woyd.ru>
2020-05-08 00:43:10 +0300

    JwtRequest: remove getters, setters, constructors use lombok

--- src/main/java/com/example/demo/model/JwtRequest.java
+++ src/main/java/com/example/demo/model/JwtRequest.java
@@ -1,31 +1,17 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class JwtRequest implements Serializable {
     private String username;
     private String password;
-
-    public JwtRequest() {}
-
-    public JwtRequest(String username, String password) {
-        this.setUsername(username);
-        this.setPassword(password);
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
